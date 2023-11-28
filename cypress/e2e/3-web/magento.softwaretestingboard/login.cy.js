2c68f953b990d1970fc8f7755664578af7fcce4b
cargibamba <magicgibsonbeat@gmail.com>
2023-11-29 06:15:28 +0700

    changes from branch

--- cypress/e2e/3-web/magento.softwaretestingboard/login.cy.js
+++ cypress/e2e/3-web/magento.softwaretestingboard/login.cy.js
@@ -12,7 +12,7 @@ describe('Verify magento functionality', () => {
   it('Succes Login', () => {
     cy.visit('https://magento.softwaretestingboard.com/customer/account/login')
     cy.get(loginPage.email).type(userData.valid.valid_email)
-    //cy.get('#email').type('magicgibsonbeat@gmail.com')..
+    //cy.get('#email').type('magicgibsonbeat@gmail.com')...
     cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass').type('MagicGibsonBeat123')
     cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2 > span').click()
     cy.url().should('include','https://magento.softwaretestingboard.com/customer/account/')
